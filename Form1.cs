e3b296ffebcf228808e69d26395bf97cd9b9568f
Aryaan Lambe <52194584+aryaanlambe@users.noreply.github.com>
2025-08-19 15:34:03 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace NFSU2_Money_Cheat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace NFSU2_Money_Cheat
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
